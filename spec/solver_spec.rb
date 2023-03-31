dd797c683ba7bec513c73a28a8fe40db42403d19
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-03-31 14:19:27 +0430

    Create test for factorial method!

---
+++ spec/solver_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../solver'
+
+describe Solver do
+  before :each do
+    @solver = Solver.new
+  end
+
+  describe 'Factorial' do
+    it 'Return 1 for factorial of 0' do
+      expect(@solver.factorial(0)).to eq(1)
+    end
+
+    it 'Return factorial' do
+      expect(@solver.factorial(2)).to eq(2)
+    end
+
+    it 'Return an error if a negative number is used' do
+      expect { @solver.factorial(-1) }.to(raise_error(StandardError))
+    end
+  end

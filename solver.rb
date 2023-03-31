d7682aee1a6ca58636aa61bf5d6d86093a2100c4
Fahim-Khan-P <fahim.tanvir2021@gmail.com>
2023-03-31 14:21:06 +0430

    Add factorial method to the Solver class!

--- solver.rb
+++ solver.rb
@@ -1,3 +1,11 @@
 class Solver
-
+  def factorial(num)
+    if num.negative?
+      raise ArgumentError, 'negative number not allowed'
+    elsif num.zero?
+      1
+    else
+      num * factorial(num - 1)
+    end
+  end
 end
